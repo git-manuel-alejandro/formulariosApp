4e42c79893a4b213fb48021002debbd7e3d1ee8b
manuel roa ojeda <manuel.roaojeda23@gmail.com>
2022-04-28 10:45:05 -0400

    commit

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+  constructor(private fb: FormBuilder) { }
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)],],
+    precio: [0, [Validators.required, Validators.min(0)],],
+    existencia: [0, [Validators.required, Validators.min(0)],],
+  })
 
-  ngOnInit(): void {
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4080ti'),
+  //   precio: new FormControl(1500),
+  //   existencia: new FormControl(5),
+
+  // })
+
+  campoEsValido(campo: string) {
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
   }
+
+
 
 }
